0ac81c82c393a0682ddfe14476bc0c927f8403ea
User <User@DESKTOP-VOCMGLJ>
2026-05-22 01:20:15 -0700

    I added comments

--- Login.cs
+++ Login.cs
@@ -13,24 +13,30 @@ namespace STD_SYSTEM
 {
     public partial class Login : Form
     {
+        //CONSTRUCTOR OF LOGIN FORM
         public Login()
         {
             InitializeComponent();
         }
+        //LOGIN BUTTON CLICK EVENT
         private void btnLogin_Click(object sender, EventArgs e)
         {
+           // SQL CONNECTION
           SqlConnection con = new SqlConnection( @"Data Source=DESKTOP-VOCMGLJ\SQLEXPRESS;Initial Catalog=STdb;Integrated Security=True;Encrypt=False");
             con.Open();
             string username = txtUsername.Text;
             string password = txtPassword.Text;
+           //SQL QUERY TO CHECK LOGIN CREDENTIALS
             SqlCommand cmd = new SqlCommand("SELECT Username, Password FROM logintab WHERE Username='" + txtUsername.Text + "'and Password='" + txtPassword.Text + "'", con);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             da.Fill(dt);
             if (dt.Rows.Count > 0)
             {
+                //GET LOGGED IN USER NAME FROM DATABASE RESULT
                 string loggedInUser = dt.Rows[0]["Username"].ToString();
                 MessageBox.Show("Login Success");
+                //OPEN MAINFORM AND PASS USERNAME
                 Form1 mainForm = new Form1(username);
                 mainForm.Show();
                 this.Hide();
@@ -39,6 +45,7 @@ namespace STD_SYSTEM
             {
                 MessageBox.Show("Invalid Login Please check user name and password");
             }
+            //CLOSE DATABASE CONNECTION
             con.Close();
         }
 
