37ddd81ceea8967ae19dc4432165624c560a3c5d
User <User@DESKTOP-VOCMGLJ>
2026-05-22 01:10:35 -0700

    I add comments

--- STDControl1.cs
+++ STDControl1.cs
@@ -14,23 +14,27 @@ namespace STD_SYSTEM
 {
     public partial class STDControl1 : UserControl
     {
+        //CONSTRUCTOR
         public STDControl1()
         {
-            InitializeComponent();
+          InitializeComponent();
         }
-
+    
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
+       //RUNS WHEN USER CONTROL LOADS 
         private void STDControl1_Load(object sender, EventArgs e)
         {
+            //CREATE DATABASE CONNECTION
             SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-VOCMGLJ\SQLEXPRESS;Initial Catalog=STdb;Integrated Security=True;Encrypt=False");
-
+            //OPEN CONNECTION
             con.Open();
+            //SQL QUERY TO FETCH ALL STUDENTS
             SqlCommand cmd = new SqlCommand("SELECT * FROM students", con);
-
+            //ADAP[TER TO RETRIEVE DATA 
             SqlDataAdapter da = new SqlDataAdapter(cmd);
 
             DataTable dt = new DataTable();
@@ -39,7 +43,7 @@ namespace STD_SYSTEM
 
             dataGridView1.DataSource = dt;
         }
-
+        //INSERT BUTTON(SAVE RECORDS )
         private void button1_Click(object sender, EventArgs e)
         {
             try
@@ -47,14 +51,14 @@ namespace STD_SYSTEM
                 using (SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-VOCMGLJ\SQLEXPRESS;Initial Catalog=STdb;Integrated Security=True;Encrypt=False"))
                 {
                     con.Open();
-
+                    //INSERT QUERY
                     SqlCommand cmd = new SqlCommand("INSERT INTO students VALUES(@studentid,@studentname,@email,@phone)", con);
 
                     cmd.Parameters.AddWithValue("@studentid", int.Parse(textBox1.Text));
                     cmd.Parameters.AddWithValue("@studentname", textBox2.Text);
                     cmd.Parameters.AddWithValue("@email", textBox3.Text);
                     cmd.Parameters.AddWithValue("@phone", textBox4.Text);
-
+                    //EXECUTE INSERT COMMAND
                     cmd.ExecuteNonQuery();
                 }
 
@@ -67,13 +71,9 @@ namespace STD_SYSTEM
             }
 
         }
+        //SHOW BUTTON(LOAD ALL RECORDS)
         private void button2_Click(object sender, EventArgs e)
         {
-
-
-
-
-
             SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-VOCMGLJ\SQLEXPRESS;Initial Catalog=STdb;Integrated Security=True;Encrypt=False");
 
             con.Open();
@@ -81,11 +81,12 @@ namespace STD_SYSTEM
 
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
-            da.Fill(dt);
-
+           
+            //FILL DATA TABLE
+//DISPLAY IN GRID
             dataGridView1.DataSource = dt;
         }
-
+        //UPDATE BUTTON
         private void button3_Click(object sender, EventArgs e)
         {
             try
@@ -93,14 +94,14 @@ namespace STD_SYSTEM
                 using (SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-VOCMGLJ\SQLEXPRESS;Initial Catalog=STdb;Integrated Security=True;Encrypt=False"))
                 {
                     con.Open();
-
+// UPDATE QUERY
                     SqlCommand cmd = new SqlCommand("UPDATE students SET studentname=@studentname, email=@email, phone=@phone WHERE studentid=@studentid", con);
-
+                    //PASS VALUES FROM TEXTBOXES
                     cmd.Parameters.AddWithValue("@studentid", int.Parse(textBox1.Text));
                     cmd.Parameters.AddWithValue("@studentname", textBox2.Text);
                     cmd.Parameters.AddWithValue("@email", textBox3.Text);
                     cmd.Parameters.AddWithValue("@phone", textBox4.Text);
-
+                    //EXECUTE UPDATE
                     cmd.ExecuteNonQuery();
                 }
 
@@ -112,7 +113,7 @@ namespace STD_SYSTEM
                 MessageBox.Show(ex.Message);
             }
         }
-
+        //DELETE BUTTON
         private void button4_Click(object sender, EventArgs e)
         {
             try
@@ -120,11 +121,11 @@ namespace STD_SYSTEM
                 using (SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-VOCMGLJ\SQLEXPRESS;Initial Catalog=STdb;Integrated Security=True;Encrypt=False"))
                 {
                     con.Open();
-
+                    //DELETE QUERY
                     SqlCommand cmd = new SqlCommand("DELETE FROM students WHERE studentid=@studentid", con);
-
+                    //GET STUDENT ID
                     cmd.Parameters.AddWithValue("@studentid", int.Parse(textBox1.Text));
-
+                    //EXECUTE DELETE
                     cmd.ExecuteNonQuery();
                 }
 
@@ -136,7 +137,7 @@ namespace STD_SYSTEM
                 MessageBox.Show(ex.Message);
             }
         }
-
+        // SEARCH BUTTON
         private void btnsearch_Click(object sender, EventArgs e)
         {
             try
@@ -144,7 +145,7 @@ namespace STD_SYSTEM
                 using (SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-VOCMGLJ\SQLEXPRESS;Initial Catalog=STdb;Integrated Security=True;Encrypt=False"))
                 {
                     con.Open();
-
+                    //SEARCH QUERY BY STUDENTID
                     SqlCommand cmd = new SqlCommand("SELECT * FROM students WHERE studentid=@studentid", con);
 
                     cmd.Parameters.AddWithValue("@studentid", int.Parse(textBox1.Text));
